15eef8ca0fbbc97e380143c8d4650d01e2677603
Victor Diego <victordieggo@gmail.com>
2017-02-11 13:17:49 -0200

    Navigation adjustments

--- assets/js/src/navigation.js
+++ assets/js/src/navigation.js
@@ -7,42 +7,26 @@ $(document).ready(function () {
 
     'use strict';
 
-    //-------------------------------------------------------------------
-    // SET VARIABLES
-    //-------------------------------------------------------------------
+    // set variables
     var nav            = $('.main-nav'),
         navBtn         = $('.nav-btn'),
-        header         = $('.main-header'),
-        page           = $('html, body'),
         parentItem     = $('.menu-item-has-children'),
+        header         = $('.main-header'),
+        page           = $('body'),
         activateScroll = $('a[href*="#"]'),
-        breakPoint     = window.innerWidth,
-        scrollPosition = $(window).scrollTop();
+        browser        = $(window);
 
-    //-------------------------------------------------------------------
-    // UPDATE WINDOW WIDTH/SCROLL POSITION
-    //-------------------------------------------------------------------
-    $(window).on('resize', function getBreakpoint() {
-        breakPoint = window.innerWidth;
-    });
-
-    $(window).on('scroll', function getScrollposition() {
-        scrollPosition = $(window).scrollTop();
-    });
-
-    //-------------------------------------------------------------------
-    // FUNCTION: ADD/REMOVE HEADER BACKGROUND
-    //-------------------------------------------------------------------
+    // add/remove header background
     function headerBackground() {
-        if (breakPoint >= 992) {
-            if (scrollPosition >= 10) {
+        if (window.innerWidth >= 992) {
+            if (browser.scrollTop() >= 15) {
                 header.addClass('bg-darker fixed-header');
             } else {
                 header.removeClass('bg-darker fixed-header');
             }
         } else {
             if (!nav.hasClass('main-nav-is-active')) {
-                if (scrollPosition > 0) {
+                if (browser.scrollTop() > 0) {
                     header.addClass('bg-darker');
                 } else {
                     header.removeClass('bg-darker');
@@ -53,31 +37,19 @@ $(document).ready(function () {
         }
     }
 
-    //-------------------------------------------------------------------
-    // FUNCTION: OPEN MOBILE NAVIGATION
-    //-------------------------------------------------------------------
-    function openNavigation() {
-        nav.addClass('main-nav-is-active');
+    // show/hide mobile navigation
+    navBtn.on('click', function () {
+        nav.toggleClass('main-nav-is-active');
         headerBackground();
-    }
+    });
 
-    //-------------------------------------------------------------------
-    // FUNCTION: CLOSE MOBILE NAVIGATION
-    //-------------------------------------------------------------------
-    function closeNavigation() {
-        nav.removeClass('main-nav-is-active');
-        headerBackground();
-    }
-
-    //-------------------------------------------------------------------
-    // FUNCTION: OPEN/CLOSE NAVIGATION SUB ITEMS
-    //-------------------------------------------------------------------
+    // navigation sub-items
     parentItem.each(function () {
         $('a:first', this).click(function (event) {
             event.preventDefault();
         });
         $(this).click(function () {
-            if (breakPoint <= 992) {
+            if (window.innerWidth <= 992) {
                 $(this).toggleClass('menu-item-is-active');
                 $('ul', this).children().click(function (event) {
                     event.stopPropagation();
@@ -86,42 +58,24 @@ $(document).ready(function () {
         });
     });
 
-    //-------------------------------------------------------------------
-    // FUNCTION: SCROLL NAVIGATION
-    //-------------------------------------------------------------------
-    function scrollNavigation() {
-        if (scrollPosition >= 100) {
-            $('section').each(function (i) {
-                if ($(this).position().top <= scrollPosition + 80) {
-                    $('a.menu-item-is-active', nav).removeClass('menu-item-is-active');
-                    $('a', nav).eq(i).addClass('menu-item-is-active');
-                }
-            });
-        } else {
-            $('a.menu-item-is-active', nav).removeClass('menu-item-is-active');
-            $('a:first', nav).addClass('menu-item-is-active');
-        }
-    }
-
-    //-------------------------------------------------------------------
-    // CALL FUNCTIONS
-    //-------------------------------------------------------------------
-    navBtn.on('click', function () {
-        if (nav.hasClass('main-nav-is-active')) {
-            closeNavigation();
-        } else {
-            openNavigation();
-        }
+    // show current section on navigation
+    browser.on('load scroll resize', function () {
+        $('section').each(function (i) {
+            if ($(this).position().top <= browser.scrollTop() + 80) {
+                $('a.menu-item-is-active', nav).removeClass('menu-item-is-active');
+                $('a', nav).eq(i).addClass('menu-item-is-active');
+            }
+        });
+        headerBackground();
     });
 
+    // scroll to target
     activateScroll.on('click', function () {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (breakPoint <= 992) {
-            nav.removeClass('main-nav-is-active');
-            if ((target.offset().top === 0) && (header.offset().top === 0)) {
-                header.removeClass('bg-darker');
-            }
+        nav.removeClass('main-nav-is-active');
+        if ((target.offset().top === 0) && (header.offset().top === 0)) {
+            header.removeClass('bg-darker');
         }
         if (target.length) {
             page.animate({
@@ -130,8 +84,5 @@ $(document).ready(function () {
             return false;
         }
     });
-
-    $(window).on('resize scroll', headerBackground);
-    $(window).on('load scroll', scrollNavigation);
 
 });
